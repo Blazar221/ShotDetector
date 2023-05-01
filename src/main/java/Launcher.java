816057c39f1c7353dfaeea5fc9a5ad7c75015090
Blazar <805781038@qq.com>
2023-05-01 14:28:57 -0700

    create video player and button bar

--- src/main/java/Launcher.java
+++ src/main/java/Launcher.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class Launcher {
+import view.VideoPlayer;
+
+public class Launcher {
+    public static void main(String[] args) {
+        VideoPlayer videoPlayer = new VideoPlayer();
+        videoPlayer.init();
+    }
 }
--- src/main/java/view/VideoPlayer.java
+++ src/main/java/view/VideoPlayer.java
@@ -1,2 +1,39 @@
-package view;public class VideoPlayer {
+package view;
+
+import uk.co.caprica.vlcj.player.component.EmbeddedMediaPlayerComponent;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class VideoPlayer {
+    EmbeddedMediaPlayerComponent mediaPlayerComponent = new EmbeddedMediaPlayerComponent();
+    JFrame frame = new JFrame();
+    JPanel contentPane = new JPanel();
+
+    JPanel buttonBar = new JPanel();
+    JButton playButton = new JButton("Play");
+    JButton pauseButton = new JButton("Pause");
+    JButton stopButton = new JButton("Stop");
+
+    public void init(){
+        frame.setBounds(new Rectangle(200, 200, 800, 600));
+        // Add all content to the frame
+        contentPane.setLayout(new BorderLayout());
+        frame.setContentPane(contentPane);
+        // Add media player in the center of content
+        mediaPlayerComponent = new EmbeddedMediaPlayerComponent();
+        contentPane.add(mediaPlayerComponent, BorderLayout.CENTER);
+        // Add buttons and init event listener, then add button to the content
+        buttonBar.add(playButton);
+        buttonBar.add(pauseButton);
+        buttonBar.add(stopButton);
+        contentPane.add(buttonBar, BorderLayout.SOUTH);
+        // Complete frame
+        frame.setBounds(new Rectangle(200, 0, 800, 600));
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        mediaPlayerComponent.mediaPlayer().media().play("file:///D:/MyDocument/Study/576MultiMedia/proj/Ready_Player_One_rgb/Ready_Player_One_rgb/InputVideo.mp4");
+    }
+    
 }
